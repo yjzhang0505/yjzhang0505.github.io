24c40acb06ecc7e32c36abee59f7d908d4cf4e1a
yjzhang0505 <150773750+yjzhang0505@users.noreply.github.com>
2023-12-25 14:43:47 +0800

    Add files via upload

---
+++ settings64.sh
@@ -0,0 +1,9 @@
+##############################################################
+# Copyright (c) 1986-2023 Xilinx, Inc.  All rights reserved. #
+##############################################################
+
+source /tools/Xilinx/DocNav/.settings64-DocNav.sh
+source /tools/Xilinx/Vivado/2022.1/.settings64-Vivado.sh
+source /tools/Xilinx/Vitis/2022.1/.settings64-Vitis.sh
+source /tools/Xilinx/Model_Composer/2022.1/.settings64-Model_Composer.sh
+source /tools/Xilinx/Vitis_HLS/2022.1/.settings64-Vitis_HLS.sh
